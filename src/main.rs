ca031cd10249cf29c9d49b49bcb07f4ffa662a30
pavanmanishd <pavanmanishd@gmail.com>
2024-09-25 16:23:51 +0530

    feat: Enable raw mode in terminal using crossterm

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::io::{self, Read};
+use crossterm::terminal::enable_raw_mode;
+use crossterm::terminal::disable_raw_mode;
+
+
 fn main() {
-    println!("Hello, world!");
+    enable_raw_mode().unwrap();
+    for b in io::stdin().bytes() {
+        let c = b.unwrap() as char;
+        println!("{}",c);
+        if c == 'q' {
+            disable_raw_mode().unwrap();
+            break;
+        }
+    }
 }
